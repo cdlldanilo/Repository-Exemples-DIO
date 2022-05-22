9df05bf18c6cddb8993fd632dfb01632d30c51b7
Danilo Cardilli <danilocardilli@gmail.com>
2022-05-22 17:40:28 -0300

    Include more file

---
+++ intellij-workspace/citiesapi-dc/src/main/java/com/github/danilo/citiesapidc/CitiesapiDcApplication.java
@@ -0,0 +1,13 @@
+package com.github.danilo.citiesapidc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitiesapiDcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitiesapiDcApplication.class, args);
+	}
+
+}
---
+++ intellij-workspace/citiesapi-dc/src/test/java/com/github/danilo/citiesapidc/CitiesapiDcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.danilo.citiesapidc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitiesapiDcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
